6bdcad930e4e73e7c48af57a7a2b818f381cdaac
Anna Komrakova <komrakova.anna@gmail.com>
2024-10-03 19:45:56 +0100

    hw2

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+export default function Feedback({ feedback, positiveFeedback }) {
+  const total = feedback.good + feedback.bad + feedback.neutral;
+
+  return (
+    <div className="feedback">
+      <p> Good: {feedback.good} </p>
+      <p> Bad: {feedback.bad} </p>
+      <p> Neutral: {feedback.neutral} </p>
+      <p> Total: {total} </p>
+      <p> Positive feedback: {positiveFeedback} % </p>
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { sourcemap: true },
+  resolve: {
+    alias: {
+      components: "/src/components",
+      data: "/src/data",
+      helpers: "/src/helpers",
+      styles: "/src/styles",
+    },
+  },
+});
